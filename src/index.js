af843b4b26547e3066584ee02879bba693c060b0
envincebal <vincentyan1@gmail.com>
2018-09-07 00:34:57 -0700

    Added webpack configuration

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import square, { add, name } from './utilities';
+import scream from './scream';
+
+console.log('index.js');
+console.log(add(32, 3));
+console.log(scream(name));
+console.log(square(10));
---
+++ src/utilities.js
@@ -0,0 +1,7 @@
+const add = (a, b) => a + b;
+
+const name = 'Vincent';
+
+const square = (x) => x * x;
+
+export { add, name, square as default }
